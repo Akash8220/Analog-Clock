7ccd6d87f00449a86cfe1b2c4d58b2e5709ae8c7
Akash8220 <103556716+Akash8220@users.noreply.github.com>
2023-04-30 20:55:36 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,76 @@
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  modeSwitch = document.querySelector(".mode-switch");
+
+if (localStorage.getItem("mode") === "Dark Mode") {
+  body.classList.add("dark");
+  modeSwitch.textContent = "Light Mode";
+}
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const isDarkMode = body.classList.contains("dark");
+  modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+  localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+});
+
+const updateTime = () => {
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+setInterval(updateTime, 1000);
+
+updateTime();
+
+setInterval(showTime, 1000);
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var date = new Date();
+  var monthName = months[date.getMonth()];
+
+  var days = new Array(7);
+  days[0] = "Sunday";
+  days[1] = "Monday";
+  days[2] = "Tuesday";
+  days[3] = "Wednesday";
+  days[4] = "Thursday";
+  days[5] = "Friday";
+  days[6] = "Saturday";
+  var day = days[date.getDay()];
+
+  am_pm = "AM";
+
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = "PM";
+  }
+  if (hour == 0) {
+    hr = 12;
+    am_pm = "AM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = hour + ":"
+    + min + ":" + sec + " " + am_pm + ", " + monthName + " " + date.getDate() + "," + " " + date.getFullYear();
+
+  document.getElementById("show-time")
+    .innerHTML = currentTime;
+}
+
+showTime();
